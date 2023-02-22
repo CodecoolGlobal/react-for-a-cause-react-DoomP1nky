7ab36e9684d0617087bc9ab8f74eb011c8b31f52
Tímea Tamasi <103537849+DoomP1nky@users.noreply.github.com>
2023-02-22 10:46:21 +0100

    add about component with description and pict

--- meows/src/App.js
+++ meows/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import AboutComponent from './components/AboutComponent';
+/* import DonationContactForm from './components/ContactComponent'; */
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <AboutComponent />
+      {/* <DonationContactForm /> */}
     </div>
   );
 }
